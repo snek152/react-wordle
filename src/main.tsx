7deb6db4563d12237da723be50411b1f553de9b3
snek152 <kakanisnehil@gmail.com>
2022-03-02 10:19:47 -0800

    adding tailwindcss and react-snap

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import { hydrate, render } from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const rootElement = document.getElementById("root");
+
+if (rootElement?.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement
+  );
+}
